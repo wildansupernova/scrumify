2aea2ed7bc1524dc3edcf8706856947868365418
dionsaputra <dionsaputra666@gmail.com>
2019-02-21 17:15:22 +0700

    Add preferenceUtils

--- app/src/main/java/crystal/scrumify/utils/ConstantUtils.java
+++ app/src/main/java/crystal/scrumify/utils/ConstantUtils.java
@@ -8,4 +8,11 @@ public class ConstantUtils {
     public static final int KANBAN_DONE_FRAG_ARG = 3;
 
     public static int REQ_PERMISSION = 10;
+
+    public static final String PREF_NAME_ARG = "pref_name";
+    public static final String PREF_TOKEN_ARG = "pref_token";
+    public static final String PREF_EMAIL_ARG = "pref_email";
+    public static final String PREF_USER_ID_ARG = "pref_user_id";
+    public static final String PREF_IS_LOGIN_ARG = "pref_is_login";
+
 }
